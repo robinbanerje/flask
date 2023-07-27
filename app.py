f256afe3df0167cfcc6d59795ef3be01b30adc60
Robin Banerjee <robinbanerje987@gmail.com>
2023-07-27 21:28:39 +0530

    added form route to calculate avarage marks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -22,6 +22,19 @@ def success(score):
 @app.route('/fail/<int:score>')
 def fail(score):
     return "The student has failed due to score: " + str(score)
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
+
+        avg_marks = (maths + science + history)/3
+
+        return render_template('form.html', score = avg_marks)
 
 if __name__=='__main__':
     app.run(debug=True)
